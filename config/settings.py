d315dc8c04faca6432ec03c3f7b998657aab8e53
Yeji Yoon <yeji@yun-yejiui-MacBookPro.local>
2023-01-25 21:00:27 +0900

    Style: 변경 사항 없음

--- config/settings.py
+++ config/settings.py
@@ -69,7 +69,7 @@ INSTALLED_APPS = [
 # DJANGO REST FRAMEWORK
 REST_FRAMEWORK = {
     "DEFAULT_AUTHENTICATION_CLASSES": [
-        'rest_framework_simplejwt.authentication.JWTAuthentication',
+        'rest_framework_simplejwt.authentication.JWTAuthentication',    
     ],
     "DEFAULT_PERMISSION_CLASSES": (
         "rest_framework.permissions.AllowAny",
@@ -187,6 +187,10 @@ STATIC_ROOT = os.path.join(BASE_DIR, "static")
 # media files
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, "static")
+
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
