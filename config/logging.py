a7eeb4609bf7be8ba08526e99acd9686c51062a9
suyeon-ai <kr.suyeon.kim@gmail.com>
2023-02-03 05:35:07 +0900

    chore: logging user id 수정, logging data 암호화

--- config/logging.py
+++ config/logging.py
@@ -1,6 +1,7 @@
 import json_log_formatter
 import hashlib
 from datetime import datetime
+from cryptography.fernet import Fernet
 
 class CustomisedJSONFormatter(json_log_formatter.JSONFormatter):
     def json_record(self, message, extra, record):
@@ -13,14 +14,20 @@ class CustomisedJSONFormatter(json_log_formatter.JSONFormatter):
                 pass
             else:
                 extra['board_id'] = int(extra['url'].replace('/api/board/', ''))
-
-            if _request.__dict__['_auth']:
-                extra['user_id'] = _request.__dict__['_auth']['user_id'] ^ 0
+            
+            if _request.user:
+                extra['user_id'] = hashlib.sha256(f"{_request.user.id}".encode('ascii')).hexdigest()
             else:
                 extra['user_id'] = None
 
-        extra['name'] = record.__dict__['name']
+        # extra['name'] = record.__dict__['name']
         extra['inDate'] = datetime.fromtimestamp(record.__dict__['created']).strftime('%Y-%m-%dT%X.%f')[:-3]+'Z'
         extra['detail'] = {'message': message, 'levelname':record.__dict__['levelname']}
         extra.pop('request', None)
-        return extra
+        # return extra
+        
+        # full log data encrypt
+        key = Fernet.generate_key()
+        fernet = Fernet(key)
+        encrypt_str = fernet.encrypt(f"{extra}".encode('ascii'))
+        return encrypt_str
